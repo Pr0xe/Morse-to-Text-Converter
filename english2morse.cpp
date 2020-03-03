8943ed4d02d457d140624a507214c06ddda36b12
Pr0xe <pr0xes_net@protonmail.ch>
2020-03-04 00:08:02 +0200

    Add files via upload

---
+++ english2morse.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+string morse(char x) {
+
+switch (x) {
+
+    case ' ':
+        return "/";
+    case 'A':
+        return ".-";
+    case 'B':
+        return "-...";
+    case 'C':
+        return "-.-.";
+    case 'D':
+        return "-..";
+    case 'E':
+        return ".";
+    case 'F':
+        return "..-.";
+    case 'G':
+        return "--.";
+    case 'H':
+        return "....";
+    case 'I':
+        return "..";
+    case 'J':
+        return ".---";
+    case 'K':
+        return "-.-";
+    case 'L':
+        return ".-..";
+    case 'M':
+        return "--";
+    case 'N':
+        return "-.";
+    case 'O':
+        return "---";
+    case 'P':
+        return ".--.";
+    case 'Q':
+        return "--.-";
+    case 'R':
+        return ".-.";
+    case 'S':
+        return "...";
+    case 'T':
+        return "-";
+    case 'U':
+        return "..-";
+    case 'V':
+        return "...-";
+    case 'W':
+        return ".--";
+    case 'X':
+        return "-..-";
+    case 'Y':
+        return "-.--";
+    case 'Z':
+        return "--..";
+    case '0':
+        return "-----";
+    case '1':
+        return ".----";
+    case '2':
+        return "..---";
+    case '3':
+        return "...--";
+    case '4':
+        return "....-";
+    case '5':
+        return ".....";
+    case '6':
+        return "-....";
+    case '7':
+        return "--...";
+    case '8':
+        return "---..";
+    case '9':
+        return "----.";
+}
+    return morse(x);
+}
+void morsenc(string s) {
+	int i, is_digit, is_upper, is_space;
+	for (i = 0; s[i]; i++){
+		is_digit = (s[i] >= '0' && s[i] <= '9'); 
+		is_upper = (s[i] >= 'A' && s[i] <= 'Z');
+		is_space = (s[i] == ' ');
+		if(!is_digit && !is_upper && !is_space){
+			cout << "No valid entry to encode!" << endl;
+			exit(1);
+		}
+	
+    	}
+    	for(int i = 0; s[i]; i++){
+	    	cout << morse(s[i]) <<" ";
+    	}
+	cout << endl;
+}
+int main() {
+    string s;
+    while(getline(cin,s)){
+    morsenc(s);}
+    return 0;
+} 
---
+++ morse2english.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+string text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+string morse[36] = {".-","-...","-.-.","-.."
+	,".","..-.","--.","....","..",".---"
+	,"-.-",".-..","--","-.","---",".--."
+	,"--.-",".-.","...","-","..-","...-"
+	,".--","-..-","-.--","--.."
+	,".----","..---","...--","....-","....."
+	,"-....","--...","---..","----.","-----"};
+//Function that collecting morse symbols
+char morse_to_text (const string flag){   
+	//Returns if the string is empty 
+	if (flag.empty())
+		return ' ';
+     //check if morse code match with letter 
+     //and return letter
+    for (int i=0; i<36; i++){
+	    if (morse[i] == flag){
+		    return text[i];}
+    }
+    return ' ';
+}
+//This function take the letter from above
+//and printing the final result with 
+//decrypted message
+void morseEnglish(){
+	string  message, token, result;
+	while(getline(cin, message)){
+	message += ' ';
+    	for (int i=0; i<message.length(); i++){
+		if (message[i] == ' '){
+			result += morse_to_text(token);
+			//Erases the contents of the string,
+	     		//which becomes an empty string
+	    		token.clear ();
+        	}
+        	else
+            		token += message[i];
+	}}
+    		cout << result << endl;  
+}
+int main(){
+	morseEnglish();
+	return 0;
+}
